67ff2e938b2e7d3fa86a292680a67154c4d5ed8a
Beatrix Mátlé <matlebeatrix@edu.bme.hu>
2022-02-08 13:17:01 +0100

    ok

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/sum', (req, res) => {
+    console.log(req.query);
+    const result = sum(Number(req.query.num1), Number(req.query.num2));
+    res.json(result);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+const sum = (a,b) => {
+    const res = a + b;
+    if (res > 10) {
+        return 'sok';
+    }
+    else if (res < 0){
+        return 0;
+    }
+    else {
+        return Math.floor(res);
+    }
+}
